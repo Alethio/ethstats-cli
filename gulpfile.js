3ddaaf627fbc4281cbd7d57da5fb23bffb2299cf
baxy <sabau.adi@gmail.com>
2019-04-02 12:19:08 +0300

    Remove coveralls from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,9 @@
-const path = require('path');
 const del = require('del');
 const gulp = require('gulp');
 const plumber = require('gulp-plumber');
 const babel = require('gulp-babel');
 const eslint = require('gulp-eslint');
 const excludeGitignore = require('gulp-exclude-gitignore');
-const coveralls = require('gulp-coveralls');
 
 // Initialize the babel transpiler so ES2015 files gets compiled when they're loaded
 require('@babel/register');
@@ -23,11 +21,6 @@ gulp.task('watch', function () {
   gulp.watch(['lib/**/*.js', 'test/**'], ['test']);
 });
 
-gulp.task('coveralls', function () {
-  return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
-    .pipe(coveralls());
-});
-
 gulp.task('clean', function () {
   return del('dist');
 });
@@ -41,4 +34,4 @@ gulp.task('babel', () => {
 
 gulp.task('prepare', gulp.series('clean', 'babel'));
 
-gulp.task('default', gulp.series('lint', 'coveralls'));
+gulp.task('default', gulp.series('lint'));
